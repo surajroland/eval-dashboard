4be73f74a1bfa04220229c1960f8ecaeb09718bd
Surajit Dutta <suraj.roland@gmail.com>
2019-09-24 22:36:13 +0200

    jenkins compatibility

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -49,9 +49,7 @@ CsvToHtmlTable = {
                     }
                 }
                 $table.append($tableBody);
-
                 $table.DataTable(datatables_options);
-
                 if (allow_download) {
                     $containerElement.append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
                 }
